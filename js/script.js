7aefea9a9302ee07021562f2fc9af3ff8ecc862f
caitreid <ctlnrd@gmail.com>
2023-02-14 15:56:24 -0500

    clean up js

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
-    $('.button-collapse').sideNav({
-      menuWidth: 300, // Default is 240
-      edge: 'right', // Choose the horizontal origin
-      closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-  }
-  );
+  $('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+  });
 });
