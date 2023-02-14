9a98e883049948576095ec287c11b1890e6ca895
caitreid <ctlnrd@gmail.com>
2023-02-14 16:35:03 -0500

    mute video to get around googles no autoplay rule

--- js/script.js
+++ js/script.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
+$(window).on('load', function(){
   $('.button-collapse').sideNav({
     menuWidth: 300, // Default is 240
     edge: 'right', // Choose the horizontal origin
     closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
   });
+
+  const video = document.querySelector('video')
+
+  video.play()
 });
+
